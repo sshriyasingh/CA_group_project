6a254284a70bd6bd1a6e2c57402f0371b6a8392f
Sanjana <sanjanavellanki01@gmail.com>
2024-11-05 21:50:56 +0100

    Revert "correct configuring setup"
    This reverts commit df2f1bb933f1c1886cd00677794c993e4f98345a.

--- build/utest-imgaos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sanjana/CA_group_project/utest-imgaos
-# Build directory: /Users/sanjana/CA_group_project/build/utest-imgaos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
